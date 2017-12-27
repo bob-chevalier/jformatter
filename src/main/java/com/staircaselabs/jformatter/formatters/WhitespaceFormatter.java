1edf7e110c8e8ab03bb4642bfb2b1ed59d31653f
Bob Chevalier <bob.chevalier@us.ibm.com>
2017-12-26 20:47:30 -0500

    Added WhitespaceFormatter

---
+++ src/main/java/com/staircaselabs/jformatter/formatters/WhitespaceFormatter.java
@@ -0,0 +1,48 @@
+package com.staircaselabs.jformatter.formatters;
+
+import com.staircaselabs.jformatter.core.FormatException;
+import com.staircaselabs.jformatter.core.TextToken;
+import com.staircaselabs.jformatter.core.TextToken.TokenType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.staircaselabs.jformatter.core.TokenUtils.findNext;
+import static com.staircaselabs.jformatter.core.TokenUtils.findNextByExclusion;
+import static com.staircaselabs.jformatter.core.TokenUtils.findPrevByExclusion;
+import static com.staircaselabs.jformatter.core.TokenUtils.tokenizeText;
+
+public class WhitespaceFormatter {
+
+    public static String format( String text ) throws FormatException {
+        List<TextToken> tokens = tokenizeText( text );
+        List<TextToken> validTokens = new ArrayList<>();
+
+        // skip leading whitespace from document
+        OptionalInt lineStart = findNextByExclusion( tokens, 0, TokenType.WHITESPACE, TokenType.NEWLINE, TokenType.EOF );
+
+        while( lineStart.isPresent() ) {
+            // find next newline token
+            int newline = findNext( tokens, lineStart.getAsInt(), TokenType.NEWLINE, TokenType.EOF )
+                    .orElseThrow( () -> new RuntimeException( "Unterminated line." ) );
+
+            // skip any whitespace that preceeds newline
+            int lineEnd = findPrevByExclusion( tokens, lineStart.getAsInt(), newline, TokenType.WHITESPACE )
+                    .orElse( newline );
+
+            IntStream.rangeClosed( lineStart.getAsInt(), lineEnd ).mapToObj( tokens::get ).forEach( validTokens::add );
+            if( lineEnd != newline ) {
+                validTokens.add( tokens.get( newline ) );
+            }
+
+            // find next non-whitespace token after previous newline
+            lineStart = findNextByExclusion( tokens, newline + 1, TokenType.WHITESPACE, TokenType.EOF );
+        }
+
+        return validTokens.stream().map( TextToken::toString ).collect( Collectors.joining() );
+    }
+
+}
